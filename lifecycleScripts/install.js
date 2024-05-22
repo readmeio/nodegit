31c836968b28ce43ad3f2dccf1db4cb692c02921
Bill Mill <bill@billmill.org>
2024-05-22 12:10:22 -0400

    wip: we don't currently have an s3 bucket
    going to try prebuildify instead

--- lifecycleScripts/install.js
+++ lifecycleScripts/install.js
@@ -24,9 +24,6 @@ module.exports = function install() {
       args.push("--debug");
     }
   }
-  else {
-    args.push("--fallback-to-build");
-  }
 
   return new Promise(function(resolve, reject) {
     const gypPath = path.join(__dirname, "..", "node_modules", "node-gyp", "bin", "node-gyp.js");
